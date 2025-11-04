0748c84cf18d87f75eeffea2440e6afe2221621b
hs-uxui <khs@jisan.com>
2025-11-04 17:41:56 +0900

    chore: 불필요한 주석 제거

--- js/switch.js
+++ js/switch.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const switchIcon = document.querySelector('.material-symbols-rounded');
 
   checkbox.addEventListener('change', () => {
-    switchIcon.textContent = ''; // 강제 clear
+    switchIcon.textContent = '';
     switchIcon.textContent = checkbox.checked ? 'check_small' : 'remove';
     console.log(switchIcon.textContent);
     console.log(checkbox.checked);
